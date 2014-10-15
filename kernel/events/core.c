039f4a29dc021f810ba6d00ff194334943d215db
Robert Bragg <robert@sixbynine.org>
2014-10-15 12:02:57 +0100

    perf: export perf_event_overflow
    To support pmu drivers in loadable modules, such as the i915 driver
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5478,6 +5478,7 @@ int perf_event_overflow(struct perf_event *event,
 {
 	return __perf_event_overflow(event, 1, data, regs);
 }
+EXPORT_SYMBOL_GPL(perf_event_overflow);
 
 /*
  * Generic software event infrastructure
