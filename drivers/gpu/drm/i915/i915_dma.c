32c8c8e89e80ed6d22883630b12c46116b39f630
Robert Bragg <robert@sixbynine.org>
2014-11-06 20:54:28 +0000

    drm/i915: Add i915 perf infrastructure
    This adds a DRM_IOCTL_I915_PERF_OPEN ioctl comparable to perf_event_open
    that opens a file descriptor for an event source.
    
    Based on our initial experience aiming to use the core perf
    infrastructure, this interface is inspired by perf, but focused on
    exposing metrics about work running on Gen graphics instead a CPU.
    
    One notable difference is that it doesn't support mmaping a circular
    buffer of samples into userspace. The currently planned use cases
    require an internal buffering that forces at least one copy of data
    which can be neatly hidden in a read() based interface.
    
    No specific event types are supported yet so perf_event_open can currently
    only get as far as returning EINVAL for an unknown event type.
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -818,6 +818,11 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 	mutex_init(&dev_priv->modeset_restore_lock);
 	mutex_init(&dev_priv->csr_lock);
 
+	/* Must at least be initialized before trying to pin any context
+	 * which i915_perf hooks into.
+	 */
+	i915_perf_init(dev);
+
 	intel_pm_setup(dev);
 
 	intel_display_crc_init(dev);
@@ -1067,6 +1072,7 @@ int i915_driver_unload(struct drm_device *dev)
 		return ret;
 	}
 
+	i915_perf_fini(dev);
 	intel_power_domains_fini(dev_priv);
 
 	intel_gpu_ips_teardown();
@@ -1255,6 +1261,7 @@ const struct drm_ioctl_desc i915_ioctls[] = {
 	DRM_IOCTL_DEF_DRV(I915_GEM_USERPTR, i915_gem_userptr_ioctl, DRM_UNLOCKED|DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(I915_GEM_CONTEXT_GETPARAM, i915_gem_context_getparam_ioctl, DRM_UNLOCKED|DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(I915_GEM_CONTEXT_SETPARAM, i915_gem_context_setparam_ioctl, DRM_UNLOCKED|DRM_RENDER_ALLOW),
+	DRM_IOCTL_DEF_DRV(I915_PERF_OPEN, i915_perf_open_ioctl, DRM_UNLOCKED|DRM_RENDER_ALLOW),
 };
 
 int i915_max_ioctl = ARRAY_SIZE(i915_ioctls);
