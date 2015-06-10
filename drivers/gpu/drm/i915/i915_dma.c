1706ddcfeb32741c73e90681fe1a46dd5d924e0a
Robert Bragg <robert@sixbynine.org>
2015-06-10 21:46:59 +0100

    drm/i915: start tracking device_info slice_mask
    The mask of slices enabled sometimes affects what Observability counters
    are exposed for a particular metric set configuration and so userspace
    needs the mask to be able to correctly normalize the raw data.
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -618,6 +618,7 @@ static void cherryview_sseu_info_init(struct drm_device *dev)
 	info = (struct intel_device_info *)&dev_priv->info;
 	fuse = I915_READ(CHV_FUSE_GT);
 
+	info->slice_mask = 1;
 	info->slice_total = 1;
 
 	if (!(fuse & CHV_FGT_DISABLE_SS0)) {
@@ -668,6 +669,7 @@ static void gen9_sseu_info_init(struct drm_device *dev)
 	ss_disable = (fuse2 & GEN9_F2_SS_DIS_MASK) >>
 		     GEN9_F2_SS_DIS_SHIFT;
 
+	info->slice_mask = s_enable;
 	info->slice_total = hweight32(s_enable);
 	/*
 	 * The subslice disable field is global, i.e. it applies
@@ -755,6 +757,7 @@ static void broadwell_sseu_info_init(struct drm_device *dev)
 
 
 	info = (struct intel_device_info *)&dev_priv->info;
+	info->slice_mask = s_enable;
 	info->slice_total = hweight32(s_enable);
 
 	/*
