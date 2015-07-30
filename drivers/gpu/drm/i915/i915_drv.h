88b54aa4c0e9170c8f103bd949218521c59dd287
Sourab Gupta <sourab.gupta@intel.com>
2015-07-30 09:55:18 +0530

    drm/i915: Introduce global id for contexts
    The current context user handles are specific to drm file instance.
    There are some usecases, which may require a global id for the contexts.
    For e.g. a system level GPU profiler tool may lean upon the global context
    ids to associate the performance snapshots with individual contexts.
    
    This global id may also be used further in order to provide a unique
    context id to hw.
    
    In this patch, the global ids are allocated from a separate cyclic idr and
    can be further utilized for any usecase described above.
    
    v2: According to Chris' suggestion, implemented a separate idr for holding
    global ids for contexts, as opposed to overloading the file specific
    ctx->user_handle for this purpose. This global id can also further be used
    wherever hw has to be programmed with ctx unique id, though this patch just
    introduces the hw global id as such.
    
    Signed-off-by: Sourab Gupta <sourab.gupta@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -873,6 +873,7 @@ struct i915_ctx_hang_stats {
 struct intel_context {
 	struct kref ref;
 	int user_handle;
+	int global_id;
 	uint8_t remap_slice;
 	struct drm_i915_private *i915;
 	int flags;
@@ -1864,6 +1865,8 @@ struct drm_i915_private {
 	struct intel_vbt_data vbt;
 
 	bool preserve_bios_swizzle;
+
+	struct idr global_ctx_idr;
 
 	/* overlay */
 	struct intel_overlay *overlay;
