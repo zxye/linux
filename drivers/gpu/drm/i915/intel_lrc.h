969f5c3a2ce6253a4146058916acaee9d88531ce
Robert Bragg <robert@sixbynine.org>
2015-09-07 17:55:36 +0100

    drm/i915: return ctx->global_id from intel_execlists_ctx_id()
    The newly added intel_context::global_id is suitable (a globally unique
    20 bit ID) for giving to the hardware as a unique context identifier.
    
    Compared to using the pinned address of a logical ring context these IDs
    are constant for the lifetime of a context whereas a context could be
    repinned at different addresses during its lifetime.
    
    Having a stable ID is useful when we need to buffer information
    associated with a context based on this ID so the association can't be
    lost. For example the OA unit writes out counter reports to a circular
    buffer tagged with this ID and we want to be able to accurately filter
    reports for a specific context, ideally without the added complexity of
    tracking context re-pinning while the OA buffer may contain reports with
    older IDs.
    
    Cc: Sourab Gupta <sourab.gupta@intel.com>
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- drivers/gpu/drm/i915/intel_lrc.h
+++ drivers/gpu/drm/i915/intel_lrc.h
@@ -93,7 +93,7 @@ struct i915_execbuffer_params;
 int intel_execlists_submission(struct i915_execbuffer_params *params,
 			       struct drm_i915_gem_execbuffer2 *args,
 			       struct list_head *vmas);
-u32 intel_execlists_ctx_id(struct drm_i915_gem_object *ctx_obj);
+u32 intel_execlists_ctx_id(struct intel_context *ctx);
 
 void intel_lrc_irq_handler(struct intel_engine_cs *ring);
 void intel_execlists_retire_requests(struct intel_engine_cs *ring);
