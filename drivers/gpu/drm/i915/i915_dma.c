f99e3b9364df7037546049d4c70977bd3ad19178
Robert Bragg <robert@sixbynine.org>
2015-09-16 21:44:00 +0100

    drm/i915: broadwell_sseu_info_init: init info->subslice_mask
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -576,6 +576,7 @@ static void broadwell_sseu_info_init(struct drm_device *dev)
 	int s_max = 2, ss_max = 3, eu_max = 8;
 	int s, ss;
 	u32 fuse2, eu_dis0, eu_dis1, eu_disable[s_max], s_enable, ss_disable;
+	u32 ss_mask;
 
 	info = (struct intel_device_info *)&dev_priv->info;
 	fuse2 = I915_READ(GEN8_FUSE2);
@@ -592,10 +593,18 @@ static void broadwell_sseu_info_init(struct drm_device *dev)
 
 	info->slice_mask = s_enable;
 	info->slice_total = hweight32(s_enable);
+
 	/*
 	 * The subslice disable field is global, i.e. it applies
-	 * to each of the enabled slices.
-	*/
+	 * to each of the enabled slices. We generalise this into
+	 * a mask covering all slices...
+	 */
+	ss_mask = ss_disable ^ ((1 << ss_max) - 1);
+	for (s = 0; s < s_max; s++) {
+	    if (s_enable & (0x1 << s))
+		info->subslice_mask |= ss_mask << (ss_max * s);
+	}
+
 	info->subslice_per_slice = ss_max - hweight32(ss_disable);
 	info->subslice_total = info->slice_total *
 			       info->subslice_per_slice;
