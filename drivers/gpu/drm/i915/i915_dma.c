40a6d817977b609bc4d90c80dd9e341fc4beb125
Robert Bragg <robert@sixbynine.org>
2015-09-16 21:50:14 +0100

    drm/i915: cherryview_sseu_info_init: init info->subslice_mask
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -665,8 +665,10 @@ static void cherryview_sseu_info_init(struct drm_device *dev)
 
 	info->slice_mask = 1;
 	info->slice_total = 1;
+	info->subslice_mask = 0;
 
 	if (!(fuse & CHV_FGT_DISABLE_SS0)) {
+		info->subslice_mask |= 0x1;
 		info->subslice_per_slice++;
 		eu_dis = fuse & (CHV_FGT_EU_DIS_SS0_R0_MASK |
 				 CHV_FGT_EU_DIS_SS0_R1_MASK);
@@ -674,6 +676,7 @@ static void cherryview_sseu_info_init(struct drm_device *dev)
 	}
 
 	if (!(fuse & CHV_FGT_DISABLE_SS1)) {
+		info->subslice_mask |= 0x2;
 		info->subslice_per_slice++;
 		eu_dis = fuse & (CHV_FGT_EU_DIS_SS1_R0_MASK |
 				 CHV_FGT_EU_DIS_SS1_R1_MASK);
