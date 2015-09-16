c9f79adbeb8029046680b5c863aaddd31b97fe5e
Robert Bragg <robert@sixbynine.org>
2015-09-16 19:18:23 +0100

    drm/i915: Add uncore mmio api for per-context registers
    Since the exponent for periodic OA counter sampling is maintained in a
    per-context register while we want to treat it as if it were global
    state we need to be able to safely issue an mmio write to a per-context
    register and affect any currently running context.
    
    We have to take some extra care in this case and this adds a utility
    api to encapsulate what's required.
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -724,6 +724,7 @@ intel_uncore_forcewake_for_reg(struct drm_i915_private *dev_priv,
 			       i915_reg_t reg, unsigned int op);
 
 struct intel_uncore_funcs {
+	int (*wait_for_rcs_busy)(struct drm_i915_private *dev_priv);
 	void (*force_wake_get)(struct drm_i915_private *dev_priv,
 							enum forcewake_domains domains);
 	void (*force_wake_put)(struct drm_i915_private *dev_priv,
@@ -758,6 +759,7 @@ struct intel_uncore {
 	struct notifier_block pmic_bus_access_nb;
 	struct intel_uncore_funcs funcs;
 
+	atomic_t hold_rcs_busy_count;
 	unsigned fifo_count;
 
 	enum forcewake_domains fw_domains;
@@ -3149,6 +3151,8 @@ static inline bool intel_vgpu_active(struct drm_i915_private *dev_priv)
 {
 	return dev_priv->vgpu.active;
 }
+int intel_uncore_begin_ctx_mmio(struct drm_i915_private *dev_priv);
+void intel_uncore_end_ctx_mmio(struct drm_i915_private *dev_priv);
 
 void
 i915_enable_pipestat(struct drm_i915_private *dev_priv, enum pipe pipe,
